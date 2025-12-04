55796ed58a57e50091b16f3e3b841e839a6beaa5
Rich Salz <rsalz@akamai.com>
2025-12-04 14:29:11 -0500

    Rewrite to use a wrapper class, mycsv

--- alsof.py
+++ alsof.py
@@ -6,66 +6,70 @@ name = "compdeps.csv"
 
 pat = re.compile(".*\[(.*)\]")
 fix = lambda s: s.replace(' ', '-').replace('-+-', '+')
+skip = lambda line: ' '.join(line).find("SKIP") > -1
 
-# Header columns
-sysdict = dict()
-syslist = []
-
-# I should really make a class that wraps the file, iterator, and
-# this kind of thing.
-def skip(line):
-    return ' '.join(line).find("SKIP") > -1
-
-# Parse open file `f` as a CSV file.
-def open_csv(f):
-    global sysdict, syslist
-    reader = csv.reader(open(name))
-    header = reader.__next__()[3:]
-    if len(syslist) == 0:
-        # Read and parse the header line.
-        for sysname in header:
+class mycsv:
+    COUNTS = 0
+    ARRAY = 1
+    def __init__(self, fname, what = COUNTS):
+        self.src = open(fname)
+        self.reader = csv.reader(self.src)
+        self.header = self.reader.__next__()[3:]
+        self.syslist = []
+        self.sysdict = dict()
+        for sysname in self.header:
             m = pat.match(sysname)
-            s = m[1] if m else sysname
-            s = fix(s)
-            sysdict[s] = 0
-            syslist.append(s)
-    return reader
+            s = fix(m[1] if m else sysname)
+            self.syslist.append(s)
+            self.sysdict[s] = 0 if what == mycsv.COUNTS else []
+    def __del__(self):
+        self.src.close()
+    # Iterator methods
+    def __iter__(self):
+        return self
+    def __next__(self):
+        while True:
+            line = self.reader.__next__()
+            if ' '.join(line).find("SKIP") == -1:
+                line[2] = fix(line[2])
+                return line
+    # with statement methods
+    def __enter__(self):
+        return self
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+    # Methods
+    def line_num(self):
+        return self.reader.line_num
 
 # Find all self-reported circular dependencies
 def self_reported():
-    with open(name) as f:
-        reader = open_csv(f)
-        with open("self-reported.txt", "w") as out:
-            for line in reader:
-                if skip(line): continue
-                me = fix(line[2])
-                if me not in sysdict:
-                    print(f"***{me} not found", file=sys.stderr)
-                line = line[3:]
-                for i in range(0, len(line)):
-                    if line[i].find(',') != -1:
-                        print(f"{me} : {syslist[i]}", file=out)
+    with (mycsv(name) as f,
+    open("self-reported.txt", "w") as out):
+        for line in f:
+            me = line[2]
+            if me not in f.sysdict:
+                print(f"***{me} not found", file=sys.stderr)
+            line = line[3:]
+            for i in range(0, len(line)):
+                if line[i].find(',') != -1:
+                    print(f"{me} : {f.syslist[i]}", file=out)
 
 # Find duplicate entries
 def find_duplicates():
     emails = dict()
-    with open(name) as f:
-        reader = open_csv(f)
-        for k in sysdict.keys():
-            sysdict[k] = []
-        for line in reader:
-            if skip(line): continue
-            me = fix(line[2])
-            sysdict[me].append(reader.line_num)
-            emails[reader.line_num] = line[1]
-    # Collect all items that appear more than once
-    dups = [ k for k in sysdict.keys() if len(sysdict[k]) > 1 ]
-    # Sort them by the number of items that mention them
-    dups.sort(key=lambda d: len(sysdict[d]))
-    with open("duplicate-reports.txt", "w") as out:
+    with (mycsv(name, mycsv.ARRAY) as f,
+    open("duplicate-reports.txt", "w") as out):
+        for line in f:
+            f.sysdict[line[2]].append(f.line_num())
+            emails[f.line_num()] = line[1]
+        # Collect all items that appear more than once
+        dups = [ k for k in f.sysdict.keys() if len(f.sysdict[k]) > 1 ]
+        # Sort them by the number of items that mention them
+        dups.sort(key=lambda d: len(f.sysdict[d]))
         for d in dups:
-            l = sysdict[d]
-            who = [ emails[n] for n in sysdict[d] ]
+            l = f.sysdict[d]
+            who = [ emails[n] for n in f.sysdict[d] ]
             print(f"{d} : {len(l)} : {l}\n\t{who}", file=out)
 
 find_duplicates()
