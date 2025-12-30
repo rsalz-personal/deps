ce7179814b6946782f0bcac8585ac844730800a7
Rich Salz <rsalz@akamai.com>
2025-12-30 14:02:01 -0500

    Use canonical names
    Also first implementation of circ ular dependency data.

--- alsof.py
+++ alsof.py
@@ -6,12 +6,95 @@ name = "compdeps.csv"
 what = []
 
 pat = re.compile(".*\[(.*)\]")
-fix = lambda s: s.replace(' ', '-').replace('-+-', '+')
 skip = lambda line: ' '.join(line).find("SKIP") > -1
 
-# A class that wraps the CSV reader class and includes file-opening
-# with (context) and iterator support
-class mycsv:
+# A dictionary mapping names from the CSV file to names that are
+# useable in MemGraph.
+renames = {
+    "Admin/BP" : "AdminBP",
+    "Akamai Cloud Pulse" : "AkamaiCloudPulse",
+    "app-aclp (stats + log)" : "app_aclp",
+    "app-blockstorage microservice" : "app_blockstorage",
+    "app-cloud-net-api microservice" : "app_cloud_net_api",
+    "app-dbaas (sg + aiven)" : "app_dbaas",
+    "app-infra microservice" : "app_infra",
+    "app-k8s microservice" : "app_k8s",
+    "app-nlb microservice" : "app_nlb",
+    "app-sre microservice" : "app_sre",
+    "Apt Cachers" : "AptCachers",
+    "Apt Repo" : "AptRepo",
+    "Block Storage" : "BlockStorage",
+    "bolt-monitor" : "BoltMonitor",
+    "BP/External API" : "BPExternalAPI",
+    "Build Box (Guest Kernels)" : "BuildBox",
+    "CF-API (billing)" : "CF_API_billing",
+    "Cloud Firewall" : "CloudFirewall",
+    "CloudIAM " : "CloudIAM",
+    "Cloud Manager" : "CloudManager",
+    "Community Site" : "CommunitySite",
+    "Compute Core (Vbin, host stack)" : "ComputeCore",
+    "Consul/Service Mesh" : "Consul_ServiceMesh",
+    "Customer Recursive Resolvers" : "CustomerRecursiveResolvers",
+    "DBaaS Analytics (being replaced by Akamai Cloud Pulse)" : "DBaaSAnalytics",
+    "Dc-turnup-testing" : "DCTurnupTesting",
+    "DDoS Alerting and Mitigation" : "DDoSAlertingandMitigation",
+    "DDoS-Routers" : "DDoSRouters",
+    "Developer Experience" : "DeveloperExperience",
+    "Dhcp-unnumbered" : "DhcpUnnumbered",
+    "Dhcp6-unnumbered" : "Dhcp6Unnumbered",
+    "Domain Manager" : "DomainManager",
+    "ELF Services" : "ELFServices",
+    "Fleet Manager" : "FleetManager",
+    "FRR-Reloader" : "FRR_Reloader",
+    "Github (inc bits)" : "Github_and_bits",
+    "github actions" : "github_actions",
+    "Hosting Database and ProxySQL" : "HostingDB_ProxySQL",
+    "Image Service (incl MIPS/Midas/Imagenext)" : "ImageService",
+    "Infra Boxes" : "InfraBoxes",
+    "Ingress Hosts" : "IngressHosts",
+    "Internal CA / SSL" : "InternalCA_SSL",
+    "kvdata + iptables" : "kvdata_iptables",
+    "Linode Jira" : "LinodeJira",
+    "Linode Login" : "LinodeLogin",
+    "Linode-Capirca" : "LinodeCapirca",
+    "Linode/goblin" : "LinodeGoblin",
+    "Lish/Glish/Weblish/etc" : "Lish_etc",
+    "LKE-E" : "LKE_E",
+    "LoadBalancers (api, cloud login, etc)" : "LoadBalancers",
+    "Log Collect" : "LogCollect",
+    "Longview (being replaced by Cloud Pulse)" : "Longview",
+    "Machine-Tools" : "MachineTools",
+    "Management Resolvers" : "ManagementResolvers",
+    "Metadata (in Backups)" : "MetadataBackups",
+    "Migration / Cloning" : "MigrationCloning",
+    "Authorative Nameservers" : "AuthorativeNameservers",
+    "Netbox-circuits" : "Netbox_Circuits",
+    "Network Config Backups" : "NetworkConfigBackups",
+    "o11y-firechief-updater" : "o11y_firechief_updater",
+    "Object Storage Gen 1" : "ObjectStorageGen1",
+    "Object Storage Gen 2" : "ObjectStorageGen2",
+    "Open Telemetry" : "OpenTelemetry",
+    "Ops Jenkins" : "OpsJenkins",
+    "osa (Object Storage API) microservice" : "ObjectStorageAPI",
+    "Outbound mail" : "OutboundMail",
+    "Prometheus + Alert Manager" : "Prometheus_AlertManager",
+    "Provisioning API" : "ProvisioningAPI",
+    "Rad-Unumbered" : "RadUnumbered",
+    "Reseller Support API" : "ResellerSupportAPI",
+    "Route Servers (Golinject Routeservers)" : "RouteServers",
+    "Smartwall TDD Systems" : "SmartwallTDDSystems",
+    "Tagg Boxes" : "TaggBoxes",
+    "VM Allocator" : "VMAllocator",
+    "Web Boxes (www)" : "WebBoxes",
+    "Net L4 LB" : "NetL4LB",
+}
+
+fix = lambda s: renames.get(s, s)
+
+
+##  A class that wraps the CSV reader class and includes file-opening
+##  with (context) and iterator support
+class CSVReader:
     COUNTS = 0
     ARRAY = 1
     def __init__(self, fname, what = COUNTS):
@@ -21,14 +104,14 @@ class mycsv:
             print(f"Can't open {fname}: {e.strerror} (errno={e.errno})")
             raise SystemExit(1)
         self.reader = csv.reader(self.src)
-        self.header = self.reader.__next__()[3:]
+        self.header = self.reader.__next__()
         self.syslist = []
-        self.sysdict = dict()
-        for sysname in self.header:
+        self.systems = dict()
+        for sysname in self.header[3:]:
             m = pat.match(sysname)
             s = fix(m[1] if m else sysname)
             self.syslist.append(s)
-            self.sysdict[s] = 0 if what == mycsv.COUNTS else []
+            self.systems[s] = 0 if what == CSVReader.COUNTS else []
     def __del__(self):
         if hasattr(self, 'src'):
             self.src.close()
@@ -52,37 +135,79 @@ class mycsv:
 
 # Find all self-reported circular dependencies
 def self_reported():
-    with (mycsv(name) as f,
+    # List of (me, i-depend-on) tuples.
+    pairs = []
+    who = dict()
+    when = dict()
+    with (CSVReader(name) as f,
     open("self-reported.txt", "w") as out):
         for line in f:
-            if self.sys not in f.sysdict:
-                print(f"***{self.sys} not found", file=sys.stderr)
+            if f.sys not in f.systems:
+                print(f"***{f.sys} not found", file=sys.stderr)
+            when[f.sys] = line[0][0:9].replace('/', '-')
+            who[f.sys] = line[1]
             line = line[3:]
             for i in range(0, len(line)):
                 if line[i].find(',') != -1:
-                    print(f"{self.sys} : {f.syslist[i]}", file=out)
+                    pairs.append((f.sys,f.syslist[i]))
+                    print(f"{f.sys} : {f.syslist[i]}", file=out)
+
+    names = [ k for (k,v) in pairs ]
+    names.extend([ v for (k,v) in pairs ])
+    names = set(names)
+
+    with open("self-reported.cypherl", "w") as mg:
+        for (i,n) in enumerate(names):
+            wn = when.get(n, 'XXX')
+            wo = who.get(n, 'XXX')
+            print(f'CREATE ({n}:System {{id: {i+1} name: "{n}", when: "{wn}", who: "{wo}"}});',
+                  file=mg)
+        for sys,dep in pairs:
+            print(f"CREATE ({sys})-[:DEPENDS_ON]->({dep});", file=mg)
+            print(f"CREATE ({dep})-[:DEPENDS_ON]->({sys});", file=mg)
 
 # Find duplicate entries
 def find_duplicates():
     emails = dict()
-    with (mycsv(name, mycsv.ARRAY) as f,
+    with (CSVReader(name, CSVReader.ARRAY) as f,
     open("duplicate-reports.txt", "w") as out):
         for line in f:
-            f.sysdict[f.sys].append(f.line_num())
+            f.systems[f.sys].append(f.line_num())
             emails[f.line_num()] = line[1]
         # Collect all items that appear more than once
-        dups = [ k for k in f.sysdict.keys() if len(f.sysdict[k]) > 1 ]
+        dups = [ k for k in f.systems.keys() if len(f.systems[k]) > 1 ]
         # Sort them by the number of items that mention them
-        dups.sort(key=lambda d: len(f.sysdict[d]))
+        dups.sort(key=lambda d: len(f.systems[d]))
         for d in dups:
-            l = f.sysdict[d]
-            who = [ emails[n] for n in f.sysdict[d] ]
+            l = f.systems[d]
+            who = [ emails[n] for n in f.systems[d] ]
             print(f"{d} : {len(l)} : {l}\n\t{who}", file=out)
 
+def merge_lines(merged, addl):
+    pass
+
+# Merge duplicate entries
 def merge():
-    with open(name) as f:
-        lines = f.readlines()
-    print(len(lines))
+    newname = 'new-' + name
+    merged = dict()
+    with CSVReader(name) as f:
+        lines = [ f.header ]
+        for line in f:
+            first = f.systems.get(f.sys, None)
+            if first is None:
+                merged[f.sys] = line
+                lines.append(line)
+            else:
+                ### MERGE FIELDS
+                merge_lines(first, line)
+                line[0] = 'SKIP MERGED ' + line[0]
+                lines.append(line)
+    with open(newname, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(lines)
+        for k,v in merged:
+            write.writerow(v)
+
 
 # Parse JCL.
 parser = argparse.ArgumentParser(
